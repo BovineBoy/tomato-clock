46baf3c1fb85134fdc12a5b0a4ebf7cd3b54eec2
BovineBoy <yikeyumi65@gmail.com>
2020-09-14 15:50:54 +0800

    feat: 🎸add eslint config

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,3 +1,10 @@
+const eslintLoader = {
+  loader: 'eslint-loader',
+  options: {
+    fix: true
+  }
+}
+
 module.exports = [
   // Add support for native node modules
   {
@@ -7,21 +14,27 @@ module.exports = [
   {
     test: /\.(m?js|node)$/,
     parser: { amd: false },
-    use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules'
-      }
-    }
+    use: [
+      {
+        loader: '@marshallofsound/webpack-asset-relocator-loader',
+        options: {
+          outputAssetBase: 'native_modules'
+        }
+      },
+      eslintLoader
+    ]
   },
   {
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: 'ts-loader',
-      options: {
-        transpileOnly: true
-      }
-    }
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      },
+      eslintLoader
+    ]
   }
 ]
